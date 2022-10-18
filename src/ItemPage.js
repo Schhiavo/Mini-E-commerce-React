154ead4c70f718113cf1172f0e63437bbf6f1d12
Victor Schiavo <victorschiavo343@gmail.com>
2022-10-18 09:47:10 -0300

    Manipulando a ItemPage.js para estruturar os itens

---
+++ src/ItemPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import PropTypes from 'prop-types'; //importando a biblioteca proptype que vai servir de auxilio nesse projeto. Para instalar a mesma, acesse o terminal e use o seguinte comando: "npm install proptypes"
+
+import './ItemPage.css';
+
+function ItemPage({items}){
+    return(
+        <ul className="ItemPage-items">
+            {items.map(item =>
+                <li key={item.id} className="ItemPage-item">
+                    {item.name}
+                </li>
+                )}
+        </ul>
+    );
+}
+
+// Essa função recebe um array items, percorre e o mapeia.
+
+
+ItemPage.propTypes = {
+items: PropTypes.array.isRequired
+} // função que garante que a função ItemPage receba apenas arrays
+
+export default ItemPage;
